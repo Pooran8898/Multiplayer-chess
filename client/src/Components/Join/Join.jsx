7d552b6338f23ddb8199aa519e6f5c3005eca68f
Pooran8898 <poorancoc@gmail.com>
2022-01-17 19:25:12 +0530

    Join component added with redirect functionality

--- client/src/Components/Join/Join.jsx
+++ client/src/Components/Join/Join.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-
+import { useContext } from "react";
+import { Datacontext } from "../../Context/Datacontext";
+import { Navigate } from "react-router-dom";
+import "./Join.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 
 export const Join = () => {
+    const { redirect, generateGameId, gameId, Setusername, username } = useContext(Datacontext);
     return (
         <>
-        <div>
-            
-        </div>
+            <div>
+                {redirect ? <Navigate to={"/game/" + gameId} /> : <>
+                    <div className="home-container">
+                        <h1>Play Chess Online with your Friends</h1>
+                        <p>Enter your name to create your game lobby!</p>
+                        <TextField id="outlined-basic" style={{ width: "15vw", marginBottom: "1%" }} label="Name" variant="outlined" required onChange={(event) => Setusername(event.target.value)} />
+                        <Button variant="contained" color="primary" onClick={(event) => username === "" ? event.preventDefault() : generateGameId()}>
+                            Enter
+                        </Button>
+                    </div>
+                </>}
+            </div>
         </>
     )
 }
--- client/src/Routes/Router.jsx
+++ client/src/Routes/Router.jsx
@@ -8,9 +8,8 @@ export const Router = () => {
     return (
         <>
             <Navbar />
-            <Join/>
             <Routes>
-                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/" element={<Join />} />
                 <Route path="/game/:gameId" element={<h1>Game</h1>} />
                 <Route path="*" element={<h1>Incorrect Page <Link to={"/"}>Click here to go to Home page</Link></h1>} />
             </Routes>
