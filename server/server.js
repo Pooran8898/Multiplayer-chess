ddcdd701ee602cc5c7ab596d779895f47b57b211
Pooran8898 <poorancoc@gmail.com>
2022-01-18 11:09:45 +0530

    Join Lobby Added in backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,42 @@
 const express = require("express");
-
+const http = require("http");
+const socketio = require("socket.io");
+const HTTP_PORT = "4000";
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+var numClients = {};
+var clientNames = {};
+var rematchCounter = 0;
+
+io.on("connected", (socket) => {
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+        numClients[socket.room]--;
+        console.log(numClients[socket.room]);
+    })
+
+    socket.on("joinGameLobby", (room) => {
+        const { gameId } = room;
+        socket.join(gameId);
+        console.log(gameId);
+        socket.room = gameId;
+        if (numClients[gameId] === undefined) {
+            numClients[gameId] = 1;
+        }
+        else {
+            numClients[gameId] += 1;
+        }
+        if (clientNames[gameId] === undefined) {
+            clientNames[gameId] = [];
+        }
+        clientNames[gameId].push(room.username);
+        console.log(clientNames[gameId]);
+    });
+    
+})
 
 
 app.listen("4000", () => {
