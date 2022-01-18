c46fe9fbf68e74d40145ace8a51650cc92b0eabf
Pooran8898 <poorancoc@gmail.com>
2022-01-18 19:02:10 +0530

    Rook constructor added

--- client/src/Pieces/Rook.js
+++ client/src/Pieces/Rook.js
@@ -7,4 +7,52 @@ export class Rook extends Piece {
         this.hasMoved = false;
         this.name = "Rook";
     }
+    handleMoved() {
+        this.hasMoved = true;
+    }
+
+    moved() {
+        return this.hasMoved;
+    }
+
+    getName() {
+        return this.name;
+    }
+    isMoveValid = (initialPos, endPos) => {
+        const mod = initialPos % 8;
+        const diff = 8 - mod;
+
+        return (
+            Math.abs(initialPos - endPos) % 8 === 0 ||
+            (endPos >= (initialPos - mod) && endPos < (initialPos + diff))
+        )
+    }
+
+    getPathIndicies = (initialPos, endPos) => {
+        const diff = Math.abs(endPos - initialPos);
+        var increment;
+        var indicies = [];
+
+        if (diff % 8 === 0) {
+            increment = 8;
+        }
+
+        else {
+            increment = 1;
+        }
+
+        if (endPos > initialPos) {
+            for (var i = initialPos + increment; i < endPos; i += increment) {
+                indicies.push(i);
+            }
+        }
+
+        else {
+            for (i = initialPos - increment; i > endPos; i -= increment) {
+                indicies.push(i);
+            }
+        }
+
+        return indicies;
+    }
 }
